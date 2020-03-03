ec5302d85aaa2f2e86c5a0b60618fe2466fa1eae
ayham <ayham.alaaeddin@gmail.com>
2020-03-03 11:42:43 +0100

    change Headers in branche2

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const logger = require('morgan');
 /** ROUTERS */
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const recordsRouter = require('./routes/records');
 
 /** INIT */
 const app = express();
@@ -15,20 +14,29 @@ const app = express();
 /** LOGGING */
 app.use(logger('dev'));
 
-
 /** REQUEST PARSERS */
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use((req,res,next) =>{
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, x-Requested-With, Content-Type, Accept"
+    )
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+    console.log('the response will be sent by the next function ...');
+    next()
+    })
+
 /** STATIC FILES*/
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 /** ROUTES */
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/records', recordsRouter);
 
 /** EXPORT PATH */
 module.exports = app;
+
