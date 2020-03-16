1f346f66f7ea7556e2baf82f2147846928288ce6
ayham <ayham.alaaeddin@gmail.com>
2020-03-16 14:24:56 +0100

    nummer 3

--- app.js
+++ app.js
@@ -1,66 +1,31 @@
-/** EXTERNAL DEPENDENCIES */
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+// Express-Bibliothek importieren
+var express = require('express');
 
-/** ROUTERS */
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const recordsRouter = require("./routes/records");
-const ordersRouter = require("./routes/orders");
-const { setCors } = require("./middleware/security");
+// Animals-Router importieren
+var animalRouter = require('./routes/animals')
 
-/** INIT */
-const app = express();
+var dbURI = "mongodb://localhost:27017/animalshelter"
 
-/** LOGGING */
-app.use(logger("dev"));
+// Mongoose-Bibliothek importieren
+var mongoose = require('mongoose');
 
-/**CONNECT TO DB */
-mongoose.connect("mongodb://localhost:27017/record-shop", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-});
+// Server erstellen
+var app = express()
 
-mongoose.connection.on("error", console.error);
-mongoose.connection.on("open", function() {
-  console.log("Database connection established...");
-});
+// Mit der Datenbank verbinden
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
 
+mongoose.connection.on('error', console.error)
 
+mongoose.connection.on('open', () => {
+    console.log('Datenbankverbindung hergestellt')
+})
 
-/** REQUEST PARSERS */
+// Parsen des Body in POST aktivieren
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(setCors);
 
-/** STATIC FILES*/
-app.use(express.static(path.join(__dirname, "public")));
+// Den Router für Animals einbinden
+app.use('/animals', animalRouter)
 
-/** ROUTES */
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/records", recordsRouter);
-app.use("/orders", ordersRouter);
-
-/** ERROR HANDLING */
-/*app.use(function(req, res, next) {
-  const error = new Error("Looks like something broke...");
-  error.status = 400;
-  next(error);
-});*/
-
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500).send({
-    error: {
-      message: err.message
-    }
-  });
-});
-
-/** EXPORT PATH */
 module.exports = app;
