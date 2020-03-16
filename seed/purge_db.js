9d592170ed4fb6d67dfa581544f3c3db7c2219c9
ayham <ayham.alaaeddin@gmail.com>
2020-03-16 12:10:15 +0100

    rs

--- seed/purge_db.js
+++ seed/purge_db.js
@@ -17,7 +17,7 @@ console.log("I shall purge all users");
     console.log("Database connection established...");
   });
 
-  console.log("I will purge all the old use...");
+  console.log("I will purge all the old users...");
 
   try {
     await User.deleteMany({});
