879e0df278eeaec176254796a35276735c793af8
ayham <ayham.alaaeddin@gmail.com>
2020-03-30 09:57:33 +0200

    06/1

--- controllers/recordsController.js
+++ controllers/recordsController.js
@@ -1,37 +1,36 @@
-var Records = require('../models/Record')
+const Record = require('../models/Record')
 
 exports.getRecords = async (req, res, next) => {
   // Schreib hier code um alle records aus der records-Collection zu holen
-  var records = await Records.find({})
+  const records = await Record.find()
   res.status(200).send(records);
 };
 
-exports.getRecord = async (req, res, next) => {
+exports.getRecord = (req, res, next) => {
   const { id } = req.params;
   // Schreib hier code um das record mit der id aus params aus der records-Collection zu holen
-  var record = await Records.findById(id)
+
   res.status(200).send(record);
 };
 
-exports.deleteRecord = async (req, res, next) => {
+exports.deleteRecord = (req, res, next) => {
   const { id } = req.params;
   // Schreib hier code um das record mit der id aus params aus der records-Collection zu löschen
-  var record = await Records.findByIdAndDelete(id)
+
   res.status(200).send(record);
 };
 
-exports.updateRecord = async (req, res, next) => {
+exports.updateRecord = (req, res, next) => {
   const { id } = req.params;
   const dt = req.body;
   // Schreib hier code um das record mit der id aus params in der records-Collection mit den Daten aus req.body zu aktualisieren
-  var record = await Records.findByIdAndUpdate(id, dt, {new: true})
+
   res.status(200).send(record);
 };
 
-exports.addRecord =async (req, res, next) => {
-  const data = req.body;
+exports.addRecord = (req, res, next) => {
+  const record = req.body;
   // Schreib hier code um die Daten des neuen record aus req.body in der records-Collection zu speichern
-  var record = new Records(data)
-  await record.save()
+
   res.status(200).send(record);
 };
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,23 +1,21 @@
-var Users = require('../models/User');
+const User = require('../models/User')
 
 exports.getUsers = async (req, res, next) => {
   // Schreib hier code um alle Kunden aus der users-Collection zu holen
-  var users = await Users.find()
-
-  res.status(200).send(users);
+  
 };
 
 exports.getUser = async (req, res, next) => {
   const { id } = req.params;
   // Schreib hier code um den Kunden mit der id aus params aus der users-Collection zu holen
-  var user = await Users.findById(id)
+  
   res.status(200).send(user);
 };
 
 exports.deleteUser = async (req, res, next) => {
   const { id } = req.params;
   // Schreib hier code um den Kunden mit der id aus params aus der users-Collection zu löschen
-  var user = await Users.findByIdAndDelete(id)
+  
   res.status(200).send(user);
 };
 
@@ -25,14 +23,14 @@ exports.updateUser = async (req, res, next) => {
   const { id } = req.params;
   const dt = req.body;
   // Schreib hier code um den User mit der id aus params in der users-Collection mit den Daten aus req.body zu aktualisieren
-  var user = await Users.findByIdAndUpdate(id, dt, {new: true})
+  
   res.status(200).send(user);
 };
 
 exports.addUser = async (req, res, next) => {
-  const data = req.body;
+  const userData = req.body;
   // Schreib hier code um die Daten des neuen Kunden aus req.body in der users-Collection zu speichern
-  var user = new Users(data)
-  await user.save()
+  
+  
   res.status(200).send(user);
 };
